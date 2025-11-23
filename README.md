33da9fd8f646b239e58111ffe30edbb8f966e8bb
isoberemko <jsoberemko@gmail.com>
2025-11-23 04:30:32 +0300

    add more symbol sets and letters options

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 Generate secure passwords in batches using predefined JSON templates.
 
+Passwords are printed to stdout only; the utility does not perform any disk writes unless you redirect output.
+
 ## How to
 
-1. Make
+1. Build
 
 ```bash
 make
@@ -15,9 +17,31 @@ cp pwizard /usr/local/bin/pwizard
 
 ### Configuration Template
 
-- `name` - password type or identifier
-- `length` - password length (minimum: 4 characters)  
-- `symbols` - enable/disable special characters (true/false, default is false)
+| Key | Value type | Default value | Description |
+| - | - | - | - |
+| name | string | | password type or identifier |
+| length | int | | password length (minimum: 4 symbols) |
+| symbols | bool | false | enable/disable safe set of special symbols |
+| symbols_extended | bool | false | enable/disable extended set of special symbols |
+| symbols_dangerous | bool | false | enable/disable dangerous set of special symbols |
+| custom_symbols_set | string | | specify custom set of symbols |
+| digits | bool | true | enable/disable digits |
+| letters | bool | true | enable/disable letters |
+| uppercase_only | bool | false | enable/disable uppercase-only letters set |
+| lowercase_only | bool | false | enable/disable lowercase-only letters set |
+
+- Special symbols are disabled by default
+- Digits are enabled by default
+- Uppercase and lowercase letters are enabled by default
+
+Password name and length specifications are required.
+
+Symbol sets cannot be combined. Priority order:
+
+1. custom_symbols_set
+2. symbols_dangerous
+3. symbols_extended
+4. symbols
 
 ### Example Configuration
 
--- pwizard.go
+++ pwizard.go
@@ -7,23 +7,39 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
 const (
-	nums    = "1234567890"
-	symbols = "!@#$%^&*()-_=+/?[]{}`~"
-	letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digits       = "1234567890"
+	lettersLower = "abcdefghijklmnopqrstuvwxyz"
+	lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letters      = lettersLower + lettersUpper
+
+	symbolsSafe      = "!@%*_-."
+	symbolsExtended  = symbolsSafe + "#$^&()=+[]{}|;:,<>?~"
+	symbolsDangerous = symbolsExtended + "`\"'\\/"
 )
 
 type Passwords []Password
 
 type Password struct {
-	Length        int    `json:"length"`
-	EnableSymbols bool   `json:"symbols"`
-	Type          string `json:"name"`
-	Charset       string
-	Password      string
+	Length                 *int    `json:"length"`
+	Name                   *string `json:"name"`
+	EnableSymbolsSafe      *bool   `json:"symbols"`
+	EnableSymbolsExtended  *bool   `json:"symbols_extended"`
+	EnableSymbolsDangerous *bool   `json:"symbols_dangerous"`
+	CustomSymbolsSet       *string `json:"custom_symbols_set"`
+	Digits                 *bool   `json:"digits"`
+	Letters                *bool   `json:"letters"`
+	UppercaseOnly          *bool   `json:"uppercase_only"`
+	LowercaseOnly          *bool   `json:"lowercase_only"`
+	Charset                string
+	Password               string
+}
+
+func ErrorCritical(err error) {
+	fmt.Println(err)
+	os.Exit(1)
 }
 
 func (pws *Passwords) PasswordsConfigure(configPath string) error {
@@ -38,15 +54,133 @@ func (pws *Passwords) PasswordsConfigure(configPath string) error {
 	return nil
 }
 
-func (pw *Password) PasswordValidate() error {
-	if pw.Length < 4 {
-		return errors.New("Password length can not be less then 4 symbols")
+func (pw *Password) PasswordConfigNormalizeDigits() {
+	if pw.Digits == nil {
+		pw.Digits = new(bool)
+		*pw.Digits = true
+	}
+}
+
+func (pw *Password) PasswordConfigNormalizeLetters() {
+	if pw.LowercaseOnly == nil {
+		pw.LowercaseOnly = new(bool)
+	}
+	if pw.UppercaseOnly == nil {
+		pw.UppercaseOnly = new(bool)
+	}
+	if pw.Letters == nil {
+		pw.Letters = new(bool)
+		*pw.Letters = true
+	}
+}
+
+func (pw *Password) PasswordConfigNormalizeSymbols() {
+	if pw.EnableSymbolsSafe == nil {
+		pw.EnableSymbolsSafe = new(bool)
+	}
+	if pw.EnableSymbolsExtended == nil {
+		pw.EnableSymbolsExtended = new(bool)
+	}
+	if pw.EnableSymbolsDangerous == nil {
+		pw.EnableSymbolsDangerous = new(bool)
+	}
+}
+
+func (pw *Password) PasswordConfigNormalize() {
+	pw.PasswordConfigNormalizeDigits()
+	pw.PasswordConfigNormalizeLetters()
+	pw.PasswordConfigNormalizeSymbols()
+}
+
+func (pw *Password) PasswordConfigValidateName() error {
+	switch {
+	case pw.Name == nil:
+		return errors.New("password configuration name is not specified")
+	case len(*pw.Name) == 0:
+		return errors.New("password configuration name must not be empty")
 	}
 	return nil
 }
 
-func AggregateString(substrings ...string) (aggregatedString string) {
-	return strings.Join(substrings, "")
+func (pw *Password) PasswordConfigValidateLength() error {
+	switch {
+	case pw.Length == nil:
+		return errors.New("password length is not specified")
+	case *pw.Length < 4:
+		return errors.New("password length must not be less than 4 characters")
+	}
+	return nil
+}
+
+func (pw *Password) PasswordConfigValidateLetters() error {
+	switch {
+	case *pw.LowercaseOnly && *pw.UppercaseOnly:
+		return errors.New("lowercase-only and uppercase-only modes cannot be enabled at the same time")
+	case !*pw.Letters && *pw.LowercaseOnly:
+		return errors.New("lowercase-only mode cannot be enabled when letters are disabled")
+	case !*pw.Letters && *pw.UppercaseOnly:
+		return errors.New("uppercase-only mode cannot be enabled when letters are disabled")
+	}
+	return nil
+}
+
+func (pw *Password) PasswordConfigValidateSymbols() error {
+	if pw.CustomSymbolsSet != nil {
+		switch {
+		case *pw.EnableSymbolsDangerous:
+			return errors.New("custom symbol set cannot be combined with the dangerous symbol set")
+		case *pw.EnableSymbolsExtended:
+			return errors.New("custom symbol set cannot be combined with the extended symbol set")
+		case *pw.EnableSymbolsSafe:
+			return errors.New("custom symbol set cannot be combined with the safe symbol set")
+		}
+	}
+	return nil
+}
+
+func (pw *Password) PasswordConfigValidate() {
+	if err := pw.PasswordConfigValidateName(); err != nil {
+		ErrorCritical(err)
+	}
+	if err := pw.PasswordConfigValidateLength(); err != nil {
+		ErrorCritical(err)
+	}
+	if err := pw.PasswordConfigValidateLetters(); err != nil {
+		ErrorCritical(err)
+	}
+	if err := pw.PasswordConfigValidateSymbols(); err != nil {
+		ErrorCritical(err)
+	}
+}
+
+func (pw *Password) CreateCharset() error {
+	if *pw.Digits {
+		pw.Charset += digits
+	}
+	if *pw.Letters {
+		switch {
+		case *pw.LowercaseOnly:
+			pw.Charset += lettersLower
+		case *pw.UppercaseOnly:
+			pw.Charset += lettersUpper
+		default:
+			pw.Charset += letters
+		}
+	}
+	switch {
+	case pw.CustomSymbolsSet != nil:
+		pw.Charset += *pw.CustomSymbolsSet
+	case *pw.EnableSymbolsDangerous:
+		pw.Charset += symbolsDangerous
+	case *pw.EnableSymbolsExtended:
+		pw.Charset += symbolsExtended
+	case *pw.EnableSymbolsSafe:
+		pw.Charset += symbolsSafe
+	}
+	if len(pw.Charset) == 0 {
+		return errors.New("character set is empty")
+	}
+	return nil
 }
 
 func CryptoPasswordGenerate(charset string, length int) (string, error) {
@@ -68,27 +202,22 @@ func main() {
 	var passwords Passwords
 
 	if err := passwords.PasswordsConfigure(*configPath); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	for i := range passwords {
-		if err := passwords[i].PasswordValidate(); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+		ErrorCritical(err)
 	}
 
 	for i := range passwords {
 		var err error
-		if passwords[i].EnableSymbols {
-			passwords[i].Charset = AggregateString(nums, symbols, letters)
-		} else {
-			passwords[i].Charset = AggregateString(nums, letters)
+
+		passwords[i].PasswordConfigNormalize()
+		passwords[i].PasswordConfigValidate()
+
+		if err = passwords[i].CreateCharset(); err != nil {
+			ErrorCritical(err)
 		}
-		if passwords[i].Password, err = CryptoPasswordGenerate(passwords[i].Charset, passwords[i].Length); err != nil {
-			fmt.Println("Can not generate password for", passwords[i].Type, ":", err)
+		if passwords[i].Password, err = CryptoPasswordGenerate(passwords[i].Charset, *passwords[i].Length); err != nil {
+			fmt.Println("Cannot generate password for", *passwords[i].Name, ":", err)
 			continue
 		}
-		fmt.Printf("%v: %v\n\n", passwords[i].Type, passwords[i].Password)
+		fmt.Printf("%v: %v\n\n", *passwords[i].Name, passwords[i].Password)
 	}
 }
